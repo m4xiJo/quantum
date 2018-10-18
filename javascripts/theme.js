e8621d6d4d154c7074ad4bb8ab9442d2e5d2508a
Max J <maxim.johansson@outlook.com>
2018-10-18 13:53:25 +0200

    Changed from SCSS injection to Sourcemaps
    Correction... this is a much better solution

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,4 +1,4 @@
-var head = document.getElementsByTagName('head')[0];
+/*var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
@@ -6,3 +6,4 @@ link.media = 'all';
 link.href = '/themes/Project-Opal/stylesheets/application.scss';
 head.appendChild(link);
 //link.onload = function() {callFunctionFromScript();}
+*/
