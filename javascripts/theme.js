1aab249bed5fba731cbcdfb41a9f1ab4b0926b1c
Max J <maxim.johansson@outlook.com>
2018-10-18 04:01:38 +0200

    fix to js script

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -2,8 +2,7 @@ var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.onload = function() {
-    callFunctionFromScript();
-}
-link.href = '/stylesheets/application.scss';
+link.media = 'all';
+link.href = '/themes/Project-Opal/stylesheets/application.scss';
 head.appendChild(link);
+//link.onload = function() {callFunctionFromScript();}
