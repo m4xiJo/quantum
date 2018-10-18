9dac16d58b4dc4d1e973d48ae5fcb440b26f7a9e
Max J <maxim.johansson@outlook.com>
2018-10-18 02:53:11 +0200

    SCSS file + injection script
    This one injects SCSS stylesheet directly into the head.

---
+++ javascripts/theme.js
@@ -0,0 +1,9 @@
+var head = document.getElementsByTagName('head')[0];
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.onload = function() {
+    callFunctionFromScript();
+}
+link.href = '/stylesheets/application.scss';
+head.appendChild(link);
