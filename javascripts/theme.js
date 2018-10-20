ec28f195056f5162926fc03c0fcb58d8d048c0b2
Max J <maxim.johansson@outlook.com>
2018-10-20 15:02:41 +0200

    Javascript code cleanup

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,53 +1,44 @@
-//Move main-menu outside parent containers
-
-
-
-//Emoji replacement script
-var allScripts = document.getElementsByTagName('script'); //Get this script path
-var lastScript = allScripts[allScripts.length-1].src.split('?')[0].replace(/\/[^\/]+\/?$/, '').replace(/\/[^\/]+\/?$/, '');
-//alert (lastScript);
-//alert(document.getElementsByTagName('script')[scripts.length-1]);
-
-var emojiPack = "GoogleBlob";
+//Global definitions
 var emojiCodes = {
   ':D': '/images/emoji/GoogleBlob/blobgrin.png',
   '>(': '/images/emoji/GoogleBlob/blobangry.png',
   'B)': '/images/emoji/GoogleBlob/blobcool.png'
 };
 
-var targetClass = "wiki";
+var allScripts = document.getElementsByTagName('script'); //Get this script path
+var lastScript = allScripts[allScripts.length-1].src.split('?')[0].replace(/\/[^\/]+\/?$/, '').replace(/\/[^\/]+\/?$/, '');
 
 window.onload = function() {
-  //var lol = document.getElementById('wrapper3').appendChild(document.getElementById('main-menu'));
-  //var lol = document.getElementById('main-menu').insertBefore(document.getElementById('main')[0]);
-  var lol2 = document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('quick-search'));
-  var lol = document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('main-menu'));
-  loopReplaceElements(targetClass, emojiPack, emojiCodes, lastScript);
+  document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('quick-search')); //Move the quick search outside parent container
+  document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('main-menu')); //Move main-menu outside parent container
+  loopReplaceElements("wiki", "GoogleBlob", emojiCodes, lastScript);
 }
 
-function replaceEmoticons(emojiPack, emojiCodes, emoji) {
 
-  let scripts = document.getElementsByTagName('script');
-  let lastScript = scripts[scripts.length - 1].src.replace(/\/[^\/]+\/?$/, '').replace(/\/[^\/]+\/?$/, '');
-  let processedURL = lastScript;
-  // a simple regex to match the characters used in the emoticons
-  return text.replace(/[:\-)D]+/g, function (match) {
-    return typeof emoticons[match] != 'undefined' ? '<img src="'+processedURL+emoticons[match]+'"/>' : match;
-  });
-}
+//Fade in effect
+document.addEventListener("DOMContentLoaded", function(e) {
+  document.getElementById("wrapper").classList.add('is-visible');
+  //document.getElementById("wrapper").classList.remove('is-visible');
+});
 
-//loopThoughElements();
+//Fade out effect
 
+//WORK IN PROGRESS...
+
+
+//Emoji replacement function
 function loopReplaceElements(targetClass, emojiPack, emojiCodes, themeRoot) {
+  let buffer;
   let elements = document.getElementsByClassName(targetClass); //Target class of containers where to replace
+    for (let i = 0; i < elements.length; i++) { //for each found element of class "wiki", execute...
+      for (let j = 0; j < Object.keys(emojiCodes).length; j++) { //for each emoji key
+        if (elements[i].innerText.includes(Object.keys(emojiCodes)[j]) && (elements[i].innerText.match(Object.keys(emojiCodes)[j]).length >= 1)) { //Detect if has duplicates
+            //For each emoji duplicate
+        }
 
-
-    for (let i = 0; i < elements.length; i++) {
-      for (let j = 0; j < Object.keys(emojiCodes).length; j++) {
         if (elements[i].innerText.includes(Object.keys(emojiCodes)[j])) {
-          alert("Match! " + elements[i].innerText);
-          let result = elements[i].innerHTML += '<img src="' + themeRoot + Object.values(emojiCodes)[j] + '"height="25px"/>';
-          //strMessage1.innerHTML = strMessage1.innerHTML.replace(/aaaaaa./g,'<a href=\"http://www.google.com/').replace(/.bbbbbb/g,'/world\">Helloworld</a>');
+          buffer = elements[i].innerText = elements[i].innerText.replace(Object.keys(emojiCodes)[j], '<img src="' + themeRoot + Object.values(emojiCodes)[j] + '"height="25px"/>');
+          //let result = elements[i].innerHTML += buffer;
         }
       }
     }
