4110453f24b602b2e05e3a9472b8762f7e279e83
Max J <maxim.johansson@outlook.com>
2018-10-20 22:01:52 +0200

    JS element existance checks before appying insert actions, to avoid errors.

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -8,20 +8,17 @@ var emojiCodes = {
 var allScripts = document.getElementsByTagName('script'); //Get this script path
 var lastScript = allScripts[allScripts.length-1].src.split('?')[0].replace(/\/[^\/]+\/?$/, '').replace(/\/[^\/]+\/?$/, '');
 
-//Fade in effect
+//Execute things once page loaded...
 document.addEventListener("DOMContentLoaded", function(e) {
+  listenToHelpClick();
   document.getElementById("wrapper").classList.add('is-visible');
-  document.getElementById('wrapper').insertAdjacentElement('afterend', document.getElementById('footer'));
-  document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('quick-search')); //Move the quick search outside parent container
-  document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('main-menu')); //Move main-menu outside parent container
+  if (document.getElementById('footer') !== null) document.getElementById('wrapper').insertAdjacentElement('afterend', document.getElementById('footer'));
+  if (document.getElementById('quick-search') !== null) document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('quick-search')); //Move the quick search outside parent container
+  if (document.getElementById('main-menu') !== null) document.getElementById('main').insertAdjacentElement('beforebegin', document.getElementById('main-menu')); //Move main-menu outside parent container
   //document.getElementById("wrapper").classList.remove('is-visible');
   loopReplaceElements("wiki", "GoogleBlob", emojiCodes, lastScript);
+
 });
-
-//Fade out effect
-
-//WORK IN PROGRESS...
-
 
 //Emoji replacement function
 function loopReplaceElements(targetClass, emojiPack, emojiCodes, themeRoot) {
@@ -32,11 +29,37 @@ function loopReplaceElements(targetClass, emojiPack, emojiCodes, themeRoot) {
         if (elements[i].innerText.includes(Object.keys(emojiCodes)[j]) && (elements[i].innerText.match(Object.keys(emojiCodes)[j]).length >= 1)) { //Detect if has duplicates
             //For each emoji duplicate
         }
-
         if (elements[i].innerText.includes(Object.keys(emojiCodes)[j])) {
           buffer = elements[i].innerText = elements[i].innerText.replace(Object.keys(emojiCodes)[j], '<img src="' + themeRoot + Object.values(emojiCodes)[j] + '"height="25px"/>');
           //let result = elements[i].innerHTML += buffer;
         }
       }
     }
+}
+
+//Inform users that they are going to Redmine.org page
+function listenToHelpClick() {
+  let currentHostname = window.location.hostname;
+  document.addEventListener('click', function (event) {
+  	if (event.target.matches('.help') && event.target.toString().match(/redmine\.org/g) && !event.target.toString().match(currentHostname)) {
+      let usrChoice = confirm("You are now heading to: " + event.target + "!\n\nRedmine.org is the official Redmine powered website meant for development of Redmine itself, and thus a completely different page from yours, dispite similarities in design. \nPlease don't get confused where you ended up later as this concerns creation of test issues and any test posts in general, which wouldn't be appropriate on Redmine.org");
+      if (usrChoice == false) event.preventDefault();
+    }
+  }, false);
+}
+
+//Local storage experiments
+function localStorageManager(key, value, read) {
+  if (read == true && localStorage.getItem(key) !== null) {
+    return key = localStorage.getItem(key);
+  }
+  else if (read == true && localStorage.getItem(key) === null)
+    return key = "Target key doesn't exist in the local storage!";
+
+  if (read == false && localStorage.getItem(key) === null) {
+    localStorage.setItem(key, value);
+  }
+  else if (read == false && localStorage.getItem(key) !== null) {
+    localStorage.setItem(key, value);
+  }
 }
